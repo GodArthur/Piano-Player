96def91b83faf8629dfa93e29ea04574da218e23
Korjon Chang-Jones <korjon.chang-jones@dawsoncollege.qc.ca>
2020-02-06 10:28:19 -0500

    Started circular array class. created unfinalized constructor

---
+++ Piano player/Piano player/CircularArray.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoPlayer
+{
+    public class CircularArray : IRingBuffer
+    {
+
+        double[] buffer;
+        int buffPosition;
+        public CircularArray(int length)
+        {
+            buffer = new double[length];
+            buffPosition = 0;
+        }
+
+        public double this[int index] => throw new NotImplementedException();
+
+        public int Length => throw new NotImplementedException();
+
+        public void Fill(double[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Shift(double value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
