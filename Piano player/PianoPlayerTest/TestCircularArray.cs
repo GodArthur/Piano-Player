82f174a94ee076ee6a1f5e03dd7fd68c5794105e
Korjon Chang-Jones <korjon.chang-jones@dawsoncollege.qc.ca>
2020-02-10 14:14:56 -0500

    reformed the strike method in the PianoWire class

---
+++ Piano player/PianoPlayerTest/TestCircularArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PianoPlayerTest
+{
+    [TestClass]
+    public class TestCircularArray
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
