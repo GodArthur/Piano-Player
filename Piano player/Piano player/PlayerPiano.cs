ea614a1aa0656dd4da7ce66fca05e0a5f20d0ea2
SuperiorSlav <ar7fedor@gmail.com>
2020-02-17 21:08:23 -0500

    Adding comments to Piano and PlayerPiano and fixing same note bbug

--- Piano player/Piano player/PlayerPiano.cs
+++ Piano player/Piano player/PlayerPiano.cs
@@ -10,26 +10,37 @@ using NAudio.Wave;
 
 namespace PianoPlayer
 {
+
+    //Author: Andrzej Fedorowicz and Korjon Chang-Jones
+    //1842644, 1837302
+    //2020-02-17
+    //The PlayerPiano class contains the main method which runs the program to play the piano
     class PlayerPiano
     {
+        //The main method will read input from a file, create a piano object and play the notes from the file
         public static void Main(string[] args)
         {
+            //Audio object is created to actually play the notes
             Audio player = new Audio();
+
+            //File is read and a new Piano object is created using the key mapping
             string[] allData = File.ReadAllLines("chopsticks.txt");
             string keys = allData[0];
             Piano piano = new Piano(keys, 44100);
             int count;
             
-            for (int i = 1; i < allData.Length; i++)
+            //For goes through all lines after key mapping
+            for (int i = 0; i < allData.Length; i++)
             {
+                //For goes through each character in the line
                 string line = allData[i];
                 for (int j = 0; j < line.Length; j++)
                 {
-                    //Console.WriteLine(line[j]);
+                    //StrikeKey is called
                     piano.StrikeKey(line[j]);
 
                     count = 1;
-                    //player.Play(piano.Play());
+                    //While loop makes sure there are enough samples to play the note
                     while(count != 0)
                     {
                         player.Play(piano.Play());
@@ -42,7 +53,7 @@ namespace PianoPlayer
                     }
                     //Console.Read();
                 }
-                Thread.Sleep(300); //delay
+                Thread.Sleep(400); //delay
 
                 
             }
